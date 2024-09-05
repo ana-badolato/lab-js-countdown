1daca6a1de02df42a1e31149df80d40e6e0aca58
Ana Badolato <badolatoana@gmail.com>
2024-09-05 18:09:08 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -7,16 +7,34 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
-
+const countDownNode = document.querySelector("#time");
+const startButtonNode = document.querySelector("#start-btn");
+startButtonNode.addEventListener("click", () => {
+  startCountdown();
+});
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
   // Your code goes here ...
+  console.log("startCountdown called!");
+  showToast("⏰ Final countdown! ⏰");
+  if (timer !== null) return;
+
+  timer = setInterval(() => {
+    remainingTime -= 1;
+    countDownNode.innerText = remainingTime;
+    startButtonNode.disabled = true;
+    if (remainingTime === 5){
+      showToast("Start the engines! 💥");
+    }else if (remainingTime === 0) {
+      clearInterval(timer);
+      timer = null;
+      //startButtonNode.disabled = false;
+      showToast("Lift off! 🚀");
+    }
+  }, 1000);
+  
 }
 
 
@@ -24,15 +42,25 @@ function startCountdown() {
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
-
   // Your code goes here ...
+  const toastNode = document.querySelector("#toast");
+  const toastMessageNode = document.querySelector("#toast-message")
+  const closeToastButton = document.querySelector("#close-toast");
+  toastNode.classList.add("show");
 
+  toastMessageNode.innerText = message; 
 
+  toastNode.classList.add("show");
 
+  const autoHideTimeout = setTimeout(() => {
+    toastNode.classList.remove("show");
+  }, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
+  closeToastButton.addEventListener("click", () => {
+    toastNode.classList.remove("show"); 
+    clearTimeout(autoHideTimeout); 
+  });
 }
